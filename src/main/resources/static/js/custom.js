91975bc47d7936b9bdd6617f6bb2b2cb0fa0ce14
sravan.charlakola <sravan.charlakola@OFI-CT1ALTSCHAR.som.w2k.state.me.us>
2018-07-11 17:00:47 -0400

    Initial Commit

---
+++ src/main/resources/static/js/custom.js
@@ -0,0 +1,21 @@
+//alert("I'm active");
+
+$(function() {
+    $(window).on('resize', function resize()  {
+        $(window).off('resize', resize);
+        setTimeout(function () {
+            var content = $('#content');
+            var top = (window.innerHeight - content.height()) / 2;
+            content.css('top', Math.max(0, top) + 'px');
+            $(window).on('resize', resize);
+        }, 50);
+    }).resize();
+});
+function page_init() {
+}
+
+function myFunction() {
+	
+		 alert("I'm active");
+ }
+
